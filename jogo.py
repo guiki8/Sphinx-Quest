16ffed3aac8f4e83b78e6874e0bc9c8d25330b7e
Guilherme <guiki53u19@gmail.com>
2024-07-31 12:17:56 -0300

    Criação do arquivo py

---
+++ jogo.py
@@ -0,0 +1,25 @@
+import pygame, sys
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+
+#Loop while = 1 frame
+while True:
+    #Define o fps (120)
+    clock.tick(120)
+    #Preenche a tela com uma cor
+    screen.fill((30, 30, 30))
+
+
+
+
+    #Registra os eventos
+    for event in pygame.event.get():
+
+        #Registra o evento de clicar no "x" da janela
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    #Atualiza a tela
+    pygame.display.update()
