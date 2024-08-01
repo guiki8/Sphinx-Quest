cca28763d2d495031ad55061db2919f141215bb4
Guilherme <guiki53u19@gmail.com>
2024-08-01 10:11:21 -0300

    menuesfinge

--- jogo.py
+++ jogo.py
@@ -1,7 +1,10 @@
 import pygame, sys
+from scripts.menu import MenuEsfinge
 pygame.init()
 
+#Criação da tela
 screen = pygame.display.set_mode((800, 600))
+#Definição dos fps
 clock = pygame.time.Clock()
 
 #Loop while = 1 frame
@@ -12,14 +15,12 @@ while True:
     screen.fill((30, 30, 30))
 
 
-
-
     #Registra os eventos
     for event in pygame.event.get():
-
         #Registra o evento de clicar no "x" da janela
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     #Atualiza a tela
     pygame.display.update()
+    MenuEsfinge()
