ba3119692bc175902351402ab944c43778483182
Guilherme <guiki53u19@gmail.com>
2024-08-01 22:47:33 -0300

    Adição do import de imagens

--- jogo.py
+++ jogo.py
@@ -1,31 +1,48 @@
 import pygame, sys
 from scripts.menu import MenuEsfinge
 from scripts.quest import Quest
-pygame.init()
+from scripts.utils import load_image, load_images
 
-#Criação da tela
-screen = pygame.display.set_mode((800, 600))
 #Definição dos fps
 clock = pygame.time.Clock()
 
-quests = []
-q1 = Quest(screen, {'num': 1 , 'dific': 2, 'pergunta':'Oq é oq é?', 'r1':'a', 'r2':'b', 'r3':'c', 'r4':'d'})
-quests.append(q1)
 
-#Loop while = 1 frame
-while True:
-    #Define o fps (120)
-    clock.tick(120)
-    #Preenche a tela com uma cor
-    screen.fill((30, 30, 30))   
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('Sphinx Quest')
 
-    q1.load_quest()
+        SCREEN_WIDTH = 960
+        SCREEN_HEIGHT = 720
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        #Dá um "zoom" na tela, pois os pixels são muito pequenos
+        
+        self.clock = pygame.time.Clock()
 
-    #Registra os eventos
-    for event in pygame.event.get():
-        #Registra o evento de clicar no "x" da janela
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    #Atualiza a tela
-    pygame.display.update()
+        self.movement = [False, False]
+
+        
+
+    def run(self):
+        #Loop while = 1 frame
+        while True:
+            #Define o fps (120)
+            clock.tick(120)
+            #Preenche a tela com uma cor
+            quests = []
+            q1 = Quest(self.screen, {'num': 1 , 'dific': 2, 'pergunta':'Essa pergunta e boa?', 'r1':'a', 'r2':'b', 'r3':'c', 'r4':'d'})
+            quests.append(q1)
+            self.screen.fill((30, 30, 30))
+
+            q1.load_quest()
+
+            #Registra os eventos
+            for event in pygame.event.get():
+                #Registra o evento de clicar no "x" da janela
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            #Atualiza a tela
+            pygame.display.update()
+
+Game().run()
--- scripts/quest.py
+++ scripts/quest.py
@@ -1,4 +1,6 @@
 import pygame
+from scripts.utils import load_image, load_images
+
 class Quest():
     #Definição dos atributos
     def __init__(self,screen, info={}):
@@ -9,12 +11,25 @@ class Quest():
         self.respostas = [info['r1'], info['r2'], info['r3'], info['r4']]
         self.screen = screen
         self.font_color = (0, 150, 200)
-        self.font_size = 15
-        self.fonte = pygame.font.SysFont('assets/pixelated.ttf', self.font_size, True, True)
+        self.font_size = 25
+        self.fonte = pygame.font.Font('assets/fonts/Pixel.ttf', self.font_size)
+        self.fonte_bold = pygame.font.Font('assets/fonts/PixelBold.ttf', self.font_size)
+        self.font_width = self.font_size*0.3
+
+        self.assets = {
+            'esfinge': load_image('esfinge_spr1.png'),
+            'button': load_image('button/1.png')
+        }
 
     def load_quest(self):
+        #Carrega a esfinge na tela                                    (height, width) (pos x, pos y)
+        self.screen.blit(pygame.transform.scale(self.assets['esfinge'], (400, 400)), (400, 50))
+
+        #Carrega o botão na tela                                    (height, width) (pos x, pos y)
+        self.screen.blit(pygame.transform.scale(self.assets['button'], (100, 100)), (200, 400))
+
         #Cria quadrado branco onde fica a pergunta
-        quest_box = pygame.Rect(200, 200, len(self.info['pergunta'])*self.font_size, 50)
+        quest_box = pygame.Rect(100, 50, len(self.info['pergunta'])*(self.font_size - self.font_width) + self.font_width, 50)
 
         #Joga na tela o quadrado
         pygame.draw.rect(self.screen, (255, 255, 255), quest_box)
@@ -26,4 +41,4 @@ class Quest():
         texto = self.fonte.render(self.pergunta, False, self.font_color)
 
         #Joga na tela o texto centralizado com o quadrado
-        self.screen.blit(texto, (quest_box.centerx - quest_box.width/4, quest_box.centery - quest_box.height/4))
+        self.screen.blit(texto, (quest_box.left + self.font_width, quest_box.centery - quest_box.height/4))
